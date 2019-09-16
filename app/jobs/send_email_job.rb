f78762c4aab0f0e122b8c2b5a15767c6436162ec
jaydipmk <jaydipmm97@gmail.com>
2019-09-16 12:58:47 +0530

    Sidekiq with active_job

--- app/jobs/send_email_job.rb
+++ app/jobs/send_email_job.rb
@@ -1,11 +1,10 @@
 # frozen_string_literal: true
 
-class SendEmailJob #< ApplicationJob
-  # queue_as :default
-  include Sidekiq::Worker
+class SendEmailJob < ApplicationJob
+  queue_as :default
 
   def perform(id)
-    @user = User.find_by(id: id)
-    SendingMailer.sending_email(@user).deliver_now
+    user = User.find_by(id: id)
+    SendingMailer.sending_email(user).deliver_later
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
-  after_create :send_email
+  after_commit :send_email, on: :create
 
   def send_email
     # this call the job perform in background
     user_id = self.id
-    SendEmailJob.perform_async(user_id)
+    SendEmailJob.perform_later(user_id)
   end
 end
